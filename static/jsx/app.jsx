f99b46853f89560ec8adb310014723b5a9a51e52
Chelsea <113374977+ChelseaLofton@users.noreply.github.com>
2023-04-10 15:58:52 -0700

    submit error, computer issues

---
+++ static/jsx/app.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div>
+            < Login/>
+        
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ static/jsx/login.jsx
@@ -0,0 +1,37 @@
+function Login() {
+    const [username, setUsername] = React.useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const response = await fetch('/api/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: new URLSearchParams({ username }),
+        });
+        const data = await response.json();
+
+        if (data.status === 'success') {
+            // Redirect to the next page or update the application state
+            console.log(data.message);
+        } else {
+            // Show an error message to the user
+            console.error(data.message);
+        }
+    };
+
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Username:
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                </label>
+                <button type="submit">Log in</button>
+            </form>
+        </div>
+    );
+}
+
+ReactDOM.render(<Login />, document.getElementById('root'));
