c6fb6b6ae3c8bd2aa7be7edd45904e59aee9c65f
Chelsea <113374977+ChelseaLofton@users.noreply.github.com>
2023-04-10 14:10:39 -0700

    app

--- app/model.py
+++ app/model.py
@@ -25,20 +25,21 @@ class Reservation(db.Model):
 
     def __repr__(self):
         return f'<Reservation {self.start_time} - {self.end_time}'
-    
 
-    def connected_to_db(flask_app, db_uri="postgresql:///project_data", echo=True):
 
-        flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-        flask_app.config['SQLALCHEMY_ECHO'] = echo
-        flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-        db.app = flask_app
-        db.init_app(flask_app)
 
-        print('Connected to the db!')
+def connected_to_db(flask_app, db_uri="postgresql:///project_data", echo=True):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    if __name__ == '__main__':
-        from server import app
-        connected_to_db(app)
-        db.create_all()
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
+
+
+if __name__ == '__main__':
+    from server import app
+    connected_to_db(app)
--- app/server.py
+++ app/server.py
@@ -1,21 +1,16 @@
 from flask import Flask
+from jinja2 import StrictUndefined
 from flask_sqlalchemy import SQLAlchemy
-from model import db, User, Reservation
+from model import db, User, Reservation, connected_to_db
 import os
-
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')
 app.jinja_env.undefined = StrictUndefined
 
-
-
-app.rout('/')
+@app.route('/')
 def index():
     return 'Hello World'
-
-
-
 
 if __name__ == "__main__":
     connected_to_db(app)
