72ec4138085bc53f025042073986989cb0323493
Chelsea <113374977+ChelseaLofton@users.noreply.github.com>
2023-04-12 11:53:11 -0700

    pikaday removed

--- static/jsx/reservations.jsx
+++ static/jsx/reservations.jsx
@@ -1,32 +1,51 @@
-
-function SearchReservations({ reservations }) {
-    const [date, setDate] = React.useState(null);
+function SearchReservations() {
+    const [date, setDate] = React.useState('');
     const [startTime, setStartTime] = React.useState('');
     const [endTime, setEndTime] = React.useState('');
-
-    React.useEffect(() => {
-        const datepicker = new Pikaday({
-            field: document.getElementById('datepicker'),
-            format: 'YYYY-MM-DD',
-            onSelect: function (date) {
-                setDate(this.getMoment().format('YYYY-MM-DD'));
-            },
-        });
-
-        return () => {
-            datepicker.destroy();
-        };
-    }, []);
+    const [availableTimes, setAvailableTimes] = React.useState([]);
 
     const handleSubmit = async (event) => {
-        // ...
+        event.preventDefault();
+
+        // Query the database for all reservations on the given date
+        const response = await fetch(`/api/reservations?date=${date}`);
+        const data = await response.json();
+        const reservations = data.reservations;
+
+        // Create a list of available time slots based on the reservations
+        const unavailableTimes = reservations.map((reservation) => ({
+            start: reservation.start_time,
+            end: reservation.end_time,
+        }));
+        const availableTimes = [];
+        for (let i = 0; i < 48; i++) {
+            const startTime = `${Math.floor(i / 2)
+                .toString()
+                .padStart(2, '0')}:${i % 2 === 0 ? '00' : '30'}`;
+            const endTime = `${Math.floor((i + 1) / 2)
+                .toString()
+                .padStart(2, '0')}:${(i + 1) % 2 === 0 ? '00' : '30'}`;
+            if (
+                !unavailableTimes.some(
+                    (reservation) =>
+                        reservation.start === startTime && reservation.end === endTime
+                )
+            ) {
+                availableTimes.push(`${startTime}-${endTime}`);
+            }
+        }
+        setAvailableTimes(availableTimes);
+
+        // TODO: Display the available time slots to the user
     };
 
-    const timeOptions = Array.from({ length: 48 }, (_, i) => {
-        const hours = Math.floor(i / 2);
-        const minutes = i % 2 === 0 ? '00' : '30';
-        return `${hours.toString().padStart(2, '0')}:${minutes}`;
-    });
+    const timeOptions = availableTimes.length
+        ? availableTimes
+        : Array.from({ length: 48 }, (_, i) => {
+            const hours = Math.floor(i / 2);
+            const minutes = i % 2 === 0 ? '00' : '30';
+            return `${hours.toString().padStart(2, '0')}:${minutes}`;
+        });
 
     return (
         <div>
@@ -34,7 +53,7 @@ function SearchReservations({ reservations }) {
             <form onSubmit={handleSubmit}>
                 <label>
                     Date:
-                    <input type="text" id="datepicker" required />
+                    <input type="date" value={date} onChange={(e) => setDate(e.target.value)} required />
                 </label>
                 <label>
                     Start Time:
