63cdf4a811723cf991e64ea7b36d07bddd88f637
Chelsea <113374977+ChelseaLofton@users.noreply.github.com>
2023-04-12 09:50:17 -0700

    happy with date and time selectors

--- app/server.py
+++ app/server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, session, url_for, j
 from jinja2 import StrictUndefined
 from flask_sqlalchemy import SQLAlchemy
 from model import db, User, Reservation, connect_to_db
+from datetime import datetime
 import os
 
 app = Flask(__name__, template_folder='/Users/chelsealofton/src/take-home/templates')
@@ -33,6 +34,54 @@ def reservations():
         return render_template('reservations.html', reservations=user_reservations)
     else:
         return redirect(url_for('login_page'))
+    
+
+
+@app.route('/reservation/search', methods=['POST'])
+def search_reservations():
+    data = request.get_json()
+    date = data['date']
+    start_time = data['startTime']
+    end_time = data['endTime']
+
+    # Get the datetime objects for the selected date and time range
+    start_datetime = datetime.strptime(f"{date} {start_time}", "%Y-%m-%d %H:%M")
+    end_datetime = datetime.strptime(f"{date} {end_time}", "%Y-%m-%d %H:%M")
+
+    # Get the current user's ID
+    user_id = session.get('user_id')
+
+    # Check if the user already has a reservation on the chosen date
+    existing_reservation = Reservation.query.filter(
+        Reservation.user_id == user_id,
+        Reservation.start_time >= start_datetime,
+        Reservation.end_time <= end_datetime
+    ).first()
+
+    if existing_reservation:
+        return jsonify({'error': 'You already have a reservation on the chosen date.'}), 400
+
+    # Find all available reservations within the specified time range
+    available_reservations = Reservation.query.filter(
+        Reservation.start_time >= start_datetime,
+        Reservation.end_time <= end_datetime
+    ).all()
+
+    if not available_reservations:
+        return jsonify({'error': 'No reservations available.'}), 404
+
+    # Convert the reservations to a JSON serializable format
+    available_reservations_json = [
+        {
+            'id': reservation.id,
+            'start_time': reservation.start_time.isoformat(),
+            'end_time': reservation.end_time.isoformat(),
+            'user_id': reservation.user_id,
+        } for reservation in available_reservations
+    ]
+
+    return jsonify(available_reservations_json)
+
 
 
 if __name__ == "__main__":
--- static/jsx/reservations.jsx
+++ static/jsx/reservations.jsx
@@ -1,12 +1,32 @@
-function SearchReservations({ reservations }){
-    const [date, setDate] = React.useState('');
+
+function SearchReservations({ reservations }) {
+    const [date, setDate] = React.useState(null);
     const [startTime, setStartTime] = React.useState('');
     const [endTime, setEndTime] = React.useState('');
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // Send the search request to the server with the selected date and time range.
+    React.useEffect(() => {
+        const datepicker = new Pikaday({
+            field: document.getElementById('datepicker'),
+            format: 'YYYY-MM-DD',
+            onSelect: function (date) {
+                setDate(this.getMoment().format('YYYY-MM-DD'));
+            },
+        });
+
+        return () => {
+            datepicker.destroy();
+        };
+    }, []);
+
+    const handleSubmit = async (event) => {
+        // ...
     };
+
+    const timeOptions = Array.from({ length: 48 }, (_, i) => {
+        const hours = Math.floor(i / 2);
+        const minutes = i % 2 === 0 ? '00' : '30';
+        return `${hours.toString().padStart(2, '0')}:${minutes}`;
+    });
 
     return (
         <div>
@@ -14,15 +34,37 @@ function SearchReservations({ reservations }){
             <form onSubmit={handleSubmit}>
                 <label>
                     Date:
-                    <input type="date" value={date} onChange={(e) => setDate(e.target.value)} required />
+                    <input type="text" id="datepicker" required />
                 </label>
                 <label>
                     Start Time:
-                    <input type="time" value={startTime} onChange={(e) => setStartTime(e.target.value)} step="1800" required />
+                    <select
+                        value={startTime}
+                        onChange={(e) => setStartTime(e.target.value)}
+                        required
+                    >
+                        <option value="">Select start time</option>
+                        {timeOptions.map((time) => (
+                            <option key={time} value={time}>
+                                {time}
+                            </option>
+                        ))}
+                    </select>
                 </label>
                 <label>
                     End Time:
-                    <input type="time" value={endTime} onChange={(e) => setEndTime(e.target.value)} step="1800" required />
+                    <select
+                        value={endTime}
+                        onChange={(e) => setEndTime(e.target.value)}
+                        required
+                    >
+                        <option value="">Select end time</option>
+                        {timeOptions.map((time) => (
+                            <option key={time} value={time}>
+                                {time}
+                            </option>
+                        ))}
+                    </select>
                 </label>
                 <button type="submit">Search</button>
             </form>
